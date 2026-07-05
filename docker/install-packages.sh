b349203d598f9a64584a001c4ea396c2ca453cc8
Derrick Neal <neald44@gmail.com>
2026-07-05 08:55:20 -0400

    started to modularize the dockerfile and adding tooling packages

---
+++ docker/install-packages.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+apt-get update
+
+apt-get install -y \
+    git \
+    curl \
+    wget \
+    jq \
+    unzip \
+    zip \
+    tree \
+    htop \
+    btop \
+    ncdu \
+    ripgrep \
+    fd-find \
+    bat \
+    fzf \
+    less \
+    vim \
+    nano \
+    tmux \
+    build-essential \
+    ca-certificates \
+    gnupg \
+    lsb-release \
+    software-properties-common \
+    python3 \
+    python3-pip \
+    python3-venv
+
+apt-get clean
+rm -rf /var/lib/apt/lists/*
